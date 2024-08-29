e081c4078731cbfd77a392f889c179c0f3f9f2f6
BionicFlight <tangdi@zjut.edu.cn>
2024-08-29 16:15:37 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# Facetracking
+# fv_tracking
+
+fly-vision(飞视)目标跟踪代码
+
+## 项目简介
+
+基于ROS(Robot Operating System)框架一个物体追踪的项目，目前处于持续开发阶段
+
+## 环境配置
+
+当前项目的运行环境为Ubuntu 16.04/18.04 LTS + ROS Kinetic/Melodic + OpenCV 3.3.1，请确保环境安装正确
+
+
+
+#### 在工作空间的src目录下输入以下命名将所有文件导入
+
+```shell
+$ git clone http://gitlab.amovauto.com/amovlab/fv_tracking.git
+```
+
+## 运行步骤
+
+### 1 命令行方式
+
+#### 1.1 构建项目
+
+在工作空间目录下使用以下命令
+
+```shell
+$ catkin_make
+```
+
+#### 1.2 运行
+
+```shell
+$ rosrun fv_tracking web_cam
+$ rosrun fv_tracking tracker_kcf
+```
+
+## 模块介绍
+
+web_cam：从摄像头中读取图片帧，发布图片帧（默认摄像头0，改为其他需要修改源码）
+
+发布（Image话题）：/camera/rgb/image_raw
+
+tracker_kcf：订阅图片帧，可以鼠标设置追踪框，显示实时追踪效果（框选跟踪）
+
+订阅（Image话题）：/camera/rgb/image_raw
+
+发布（Pose2D话题，距离图像中心点的像素误差x,y）：/vision/error_pixels
+
+
---
+++ nodes/web_cam.cpp
@@ -0,0 +1,79 @@
+//Capture Camera View---advanced by Weijie Jin -----------------------------------------
+//2021年09月06日18:31:55.
+// email: 17758373920@163.com
+//wechat: xiaofuxing
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <geometry_msgs/Pose.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/aruco.hpp>
+#include "id_management.h"
+
+using namespace std;
+using namespace cv;
+
+
+image_transport::Publisher image_pub;
+//设置图像大小
+cv::Size image_size = Size(640, 360);
+
+int main(int argc, char **argv)
+{
+    int cap_flag=0;
+    ros::init(argc, argv, "web_cam");
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+    ros::Rate loop_rate(30);
+    // 在这里修改发布话题名称
+    image_pub = it.advertise("/camera/rgb/image_raw", 1);
+    // 用系统默认驱动读取摄像头0，使用其他摄像头ID，请在这里修改
+    cv::VideoCapture cap(4);
+
+    // 设置摄像头分辨率
+     cap.set(CAP_PROP_FRAME_WIDTH, 640);
+     cap.set(CAP_PROP_FRAME_HEIGHT, 360);
+
+
+
+    cv::Mat frame;
+    // 设置全屏
+    // namedWindow("web_cam frame", CV_WINDOW_NORMAL);
+    // setWindowProperty("web_cam frame", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+
+    sensor_msgs::ImagePtr msg;
+    int id = 0;
+    while (ros::ok())
+  {
+
+         cap >> frame;
+        if (!frame.empty())   
+
+        {
+            if (0 == id)
+            {
+                cout << "IM RESOLUTION: " << frame.cols << " x " << frame.rows << "  " << frame.channels() << frame.size << endl;
+            }
+            //添加ID
+            frame = IDManagement::add_id(id, frame);
+            id = id+1;
+            // 改变图像大小并显示图片
+            resize(frame, frame, image_size);
+            // imshow("web_cam frame", frame);
+            waitKey(5);
+            // 设置图像帧格式->bgr8
+            msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+            // 将图像通过话题发布出去
+            image_pub.publish(msg);
+        }
+        ros::spinOnce();
+        // 按照设定的帧率延时，ros::Rate loop_rate(30)
+        loop_rate.sleep();
+    }
+         cap.release();
+
+
+}
